b4e9d8cac22b726fcfa30b8d99b9264c254aeb25
lvp <lsspkk@gmail.com>
2020-06-28 22:22:26 +0300

    Added Kanta-Häme/Pirkanmaa region

--- backend/server.js
+++ backend/server.js
@@ -13,37 +13,37 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 // https://github.com/request/request
 app.get('/query', (req, res) => {
-	var keyword = encodeURIComponent(req.query.keyword)
-	console.log("request: /query keyword:", keyword, "category:", req.query.category, "onsale:", req.query.onsale, "free:", req.query.free, "page:", req.query.page)
+  var keyword = encodeURIComponent(req.query.keyword)
+  console.log('request: /query ', req.query)
 
-	var url = 'https://www.tori.fi/pirkanmaa?q=' + keyword + '&cg=' + req.query.category + '&w=111'
-	if (req.query.onsale == 'true') url += '&st=s'
-	if (req.query.free == 'true') url += '&st=g'
-	if (req.query.page > '1') url += '&o=' + req.query.page
+  const region = req.query.region === 'kantahame' ? 'uusimaa?w=117' : 'pirkanmaa?w=111'
+  var url = 'https://www.tori.fi/' + region + '&q=' + keyword + '&cg=' + req.query.category
+  if (req.query.onsale == 'true') url += '&st=s'
+  if (req.query.free == 'true') url += '&st=g'
+  if (req.query.page > '1') url += '&o=' + req.query.page
 
-	url += '&ca=18&l=0&md=th'
+  url += '&ca=18&l=0&md=th'
 
-	console.log('tori url :', url)
+  console.log('tori url :', url)
 
-	request({
-			method: 'GET',
-			uri: url,
-			// uri: 'https://www.tori.fi/pirkanmaa?q='+keyword+'&cg='+req.query.category+'&w=111&st=s&st=k&st=g&c=5036&ca=18&l=0&md=th',
-			// uri: 'https://www.tori.fi/pirkanmaa?q=n%E4ytt%F6&cg=5030&w=111&st=s&st=k&st=g&c=5036&ca=18&l=0&md=th',
-			encoding: 'latin1'
-		},
-		(error, response, body) => {
-			if (error)
-				console.log("tori error: ", error)
-			else if (response && response.statusCode === 200) {
-				//fs.writeFileSync('./loaded_tori_page.html', "<!--hi-->\n" + response.body)
-				console.log("tori response http code: ", response.statusCode)
-				res.send(parse(body))
-			} else {
-				console.log("tori response http code: ", response.statusCode)
-			}
-
-		})
+  request({
+    method: 'GET',
+    uri: url,
+    // https://www.tori.fi/uusimaa?q=ikkunoita&cg=0&w=117&st=s&st=k&st=u&st=h&st=g&ca=18&l=0&md=th
+    // https://www.tori.fi/uusimaa?q=ikkunoita&cg=0&w=118&st=s&st=k&st=u&st=h&st=g&ca=18&l=0&md=th
+    // uri: 'https://www.tori.fi/pirkanmaa?q='+keyword+'&cg='+req.query.category+'&w=111&st=s&st=k&st=g&c=5036&ca=18&l=0&md=th',
+    // uri: 'https://www.tori.fi/pirkanmaa?q=n%E4ytt%F6&cg=5030&w=111&st=s&st=k&st=g&c=5036&ca=18&l=0&md=th',
+    encoding: 'latin1'
+  },
+  (error, response, body) => {
+    if (error) { console.log('tori error: ', error) } else if (response && response.statusCode === 200) {
+      // fs.writeFileSync('./loaded_tori_page.html', "<!--hi-->\n" + response.body)
+      console.log('tori response http code: ', response.statusCode)
+      res.send(parse(body))
+    } else {
+      console.log('tori response http code: ', response.statusCode)
+    }
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
